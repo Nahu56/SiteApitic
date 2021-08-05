4f881cac2da19dc442ac31799bf850818183f791
Nahu56 <60613115+Nahu56@users.noreply.github.com>
2021-08-05 17:47:07 +0200

    Master

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="../css/style.css" rel="stylesheet">
+        
+        <title>APITIC</title>
+    </head>
+    <body>
+        <h1>Guilde de <br>TOM</h1>
+        <table class="table-primary">
+            <tr>
+                <td>Pseudonyme</td>
+                <td>Race</td>
+                <td>Classe</td>
+                <td>Spécialisation</td>
+                <td>Armure</td>
+                <td>Propriétaire</td>
+            </tr>
+        </table>
+    </body>
+</html>
